9e690c87c538bde13157738da2cc7a250d0db4f8
AnkoSkl <anko136660422@gmail.com>
2019-06-01 12:23:08 +0300

    Transform ast to json

--- compiler.py
+++ compiler.py
@@ -4,22 +4,46 @@ from antlr_py.CLexer import CLexer
 from antlr_py.CParser import CParser
 import json
 import argparse
-from pycparser import c_parser, c_ast
+from pycparser import c_parser, c_ast, parse_file
+import sys
 
 
-def handleExpression(expr, names, tabs=""):
+def to_screen(tree, buf=sys.stdout, attrnames=False, nodenames=False, showcoord=False, _my_node_name=None):
+
     children = {}
-    for child in expr.getChildren():
-        if isinstance(child, antlr4.tree.Tree.TerminalNode):
-            children[child.getText()] = names[child.symbol.type-1]
+    if nodenames and _my_node_name is not None:
+        str = tree.__class__.__name__+ ' <' + _my_node_name + '>: '
+    else:
+        str = tree.__class__.__name__+ ': '
+
+    if tree.attr_names:
+        if attrnames:
+            nvlist = [(n, getattr(tree,n)) for n in tree.attr_names]
+            attrstr = ', '.join('%s=%s' % nv for nv in nvlist)
         else:
-            children[type(child).__name__] = handleExpression(child, names, tabs + '-')
-    return children
+            vlist = [getattr(tree, n) for n in tree.attr_names]
+            attrstr = ', '.join('%s' % v for v in vlist)
+        str = str + attrstr
+
+    if showcoord:
+        str = str + ' (at %s)' % tree.coord
+
+    for (child_name, child) in tree.children():
+        children[child_name] = to_screen(
+                                        child,
+                                        buf,
+                                        attrnames=attrnames,
+                                        nodenames=nodenames,
+                                        showcoord=showcoord,
+                                        _my_node_name=child_name)
+    result = {}
+    result[str] = children
+    return result
 
 
 def main():
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("--input_file", type=str, default="samples/simple.c")
+    argparser.add_argument("--input_file", type=str, default="samples/factorial.c")
     args = argparser.parse_args()
 
     input_file = args.input_file
@@ -34,12 +58,13 @@ def main():
         stream = CommonTokenStream(lexer)
         parser = CParser(stream)
         tree = parser.compilationUnit()
-        tree = handleExpression(tree, lexer.ruleNames)
+        #tree = handleExpression(tree, lexer.ruleNames)
         pparser = c_parser.CParser()
         ast = pparser.parse(code)
         ast.show()
+        dictr = to_screen(ast)
         with open(output_json, "w") as output_file:
-            output_file.write(json.dumps(tree))
+            output_file.write(json.dumps(dictr))
 
 
 if __name__ == '__main__':
--- samples/factorial.c
+++ samples/factorial.c
@@ -1,5 +1,4 @@
-
-int main(int argc, char* argv[])
+int main()
 {
     int n, i;
     unsigned long long factorial = 1;
@@ -7,14 +6,13 @@ int main(int argc, char* argv[])
     printf("Enter an integer: ");
     scanf("%d", &n);
 
-    // show error if the user enters a negative integer
     if (n < 0)
         printf("Error! Factorial of a negative number doesn't exist.");
     else
     {
         for (i = 1; i <= n; ++i)
         {
-            factorial *= i; // factorial = factorial*i;
+            factorial *= i; 
         }
         printf("Factorial of %d = %llu", n, factorial);
     }
